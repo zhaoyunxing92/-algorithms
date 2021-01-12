29070cf461186995cf89ca6e95de44d49ae79729
zhaoyunxing <wb-zyx735784@alibaba-inc.com>
2021-01-12 11:42:07 +0800

    add:添加次数统计

--- sort/bubble/bubble.go
+++ sort/bubble/bubble.go
@@ -1,6 +1,9 @@
 package bubble
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 //ViolenceBubbleSort:暴力排序
 func ViolenceBubbleSort(array []int) time.Duration {
@@ -17,27 +20,31 @@ func ViolenceBubbleSort(array []int) time.Duration {
 	return time.Now().Sub(start)
 }
 
-
 //BubbleSort:排序过的数字就过滤，所以长度是一个动态的
 func BubbleSort(array []int) time.Duration {
 	start := time.Now()
+	count := 0 //记录比较次数
 	for end := len(array) - 1; end > 0; end-- {
 		for begin := 1; begin <= end; begin++ {
+			count++
 			if array[begin] < array[begin-1] {
 				array[begin], array[begin-1] = array[begin-1], array[begin]
 			}
 		}
 	}
+	fmt.Printf("[BubbleSort]数组长度：%d,比较次数：%d \n", len(array), count)
 	return time.Now().Sub(start)
 }
 
-//BubbleSort2:假如给你的数据已经有序就不用找排序了
+//BubbleSort2:假如给你的数据经过某次已经有序了
 func BubbleSort2(array []int) time.Duration {
 	start := time.Now()
+	count := 0 //记录比较次数
 	for end := len(array) - 1; end > 0; end-- {
 		//标记是否排序过
 		sorted := true
 		for begin := 1; begin <= end; begin++ {
+			count++
 			//能进来说明需要排序了
 			if array[begin] < array[begin-1] {
 				array[begin], array[begin-1] = array[begin-1], array[begin]
@@ -48,15 +55,18 @@ func BubbleSort2(array []int) time.Duration {
 			break
 		}
 	}
+	fmt.Printf("[BubbleSort2]数组长度：%d,比较次数：%d \n", len(array), count)
 	return time.Now().Sub(start)
 }
 
 //BubbleSort3:假如给你的数据已经有序就不用找排序了
 func BubbleSort3(array []int) time.Duration {
 	start := time.Now()
+	count := 0 //记录比较次数
 	for end := len(array) - 1; end > 0; end-- {
 		index := 1
 		for begin := 1; begin <= end; begin++ {
+			count++
 			if array[begin] < array[begin-1] {
 				array[begin], array[begin-1] = array[begin-1], array[begin]
 				index = begin
@@ -64,5 +74,16 @@ func BubbleSort3(array []int) time.Duration {
 		}
 		end = index
 	}
+	fmt.Printf("[BubbleSort3]数组长度：%d,比较次数：%d \n", len(array), count)
 	return time.Now().Sub(start)
+}
+
+//MaxDepth: sort排序深度算法
+func MaxDepth(size int) int {
+	fmt.Println("size=", size)
+	var depth int
+	for i := size; i > 0; i >>= 1 {
+		depth++
+	}
+	return depth * 2
 }
--- tests/bubble_test.go
+++ tests/bubble_test.go
@@ -7,7 +7,6 @@ import (
 	"testing"
 )
 
-
 //TestViolenceBubbleSort:暴力排序
 func TestViolenceBubbleSort(t *testing.T) {
 	var seed = tools.Random(4, 40, 5)
@@ -23,4 +22,37 @@ func TestBubbleSort(t *testing.T) {
 	fmt.Println("冒泡排序前：", seed)
 	bubble.BubbleSort(seed)
 	fmt.Println("冒泡排序后：", seed)
+}
+
+func TestBubbleSort2(t *testing.T) {
+	var seed = tools.Random(4, 40, 5)
+	fmt.Println("冒泡排序前：", seed)
+	bubble.BubbleSort2(seed)
+	fmt.Println("冒泡排序后：", seed)
+}
+
+func TestBubbleSort3(t *testing.T) {
+	var seed = tools.Random(4, 40, 5)
+	fmt.Println("冒泡排序前：", seed)
+	bubble.BubbleSort3(seed)
+	fmt.Println("冒泡排序后：", seed)
+}
+
+//TestBubbleSortCount:比较几个算法的次数
+func TestBubbleSortCount(t *testing.T) {
+	seed := tools.Random(4, 40, 10)
+	fmt.Println("冒泡排序前：", seed)
+	seed1 := seed[:]
+	seed2 := seed[:]
+	seed3 := seed[:]
+	bubble.BubbleSort(seed1)
+	bubble.BubbleSort2(seed2)
+	bubble.BubbleSort3(seed3)
+	fmt.Println("冒泡排序后：", seed1)
+}
+
+//TestMaxDepth:计算深度
+func TestMaxDepth(t *testing.T) {
+	depth := bubble.MaxDepth(1 << 5)
+	fmt.Println(depth)
 }
