511be60fb954df9cf529464ae176d948959bd90c
zhaoyunxing <wb-zyx735784@alibaba-inc.com>
2021-01-18 10:06:39 +0800

    up:添加次数统计

--- sort/selection/selection.go
+++ sort/selection/selection.go
@@ -8,6 +8,7 @@ import (
 /**
 根据最大值排序
 */
+//SelectionMaxSort: 选择最大的值交换
 func SelectionMaxSort(array []int) time.Duration {
 	start := time.Now()
 	count := 0 //记录比较次数
@@ -25,4 +26,36 @@ func SelectionMaxSort(array []int) time.Duration {
 	fmt.Printf("[SelectionMaxSort]数组长度：%d,比较次数：%d \n", len(array), count)
 	fmt.Println("[SelectionMaxSort]", array)
 	return time.Now().Sub(start)
+}
+
+//SelectionMinSort:选择最小
+func SelectionMinSort(array []int) {
+	count := 0 //记录比较次数
+	for begin := 0; begin < len(array); begin++ {
+		minIdx := begin
+		for end := begin + 1; end < len(array); end++ {
+			count++
+			if array[end] < array[minIdx] {
+				minIdx = end
+			}
+			fmt.Println("最小指针：", minIdx)
+		}
+		array[minIdx], array[begin] = array[begin], array[minIdx]
+	}
+	fmt.Printf("[SelectionMinSort]数组长度：%d,比较次数：%d \n", len(array), count)
+	fmt.Println("[SelectionMinSort]", array)
+}
+
+func Max(array []int) {
+
+	for start := len(array) - 1; start > 0; start-- {
+		max := 0
+		for end := 1; end <= start; end++ {
+			if array[max] < array[end] {
+				max = end
+			}
+		}
+		array[start], array[max] = array[max], array[start]
+	}
+	fmt.Println("[max]", array)
 }
--- tests/selection_test.go
+++ tests/selection_test.go
@@ -3,13 +3,26 @@ package tests
 import (
 	"fmt"
 	"github.com/zhaoyunxing92/algorithms/sort/selection"
-	"github.com/zhaoyunxing92/algorithms/tools"
 	"testing"
 )
 
 func TestSelectionMaxSort(t *testing.T) {
-	var seed = tools.Random(4, 40, 5)
+	//seed := tools.Random(4, 40, 5)
+	seed := []int{2, 3, 1, 4, 5}
 	fmt.Println("选择排序排序前：", seed)
 	selection.SelectionMaxSort(seed)
-	fmt.Println("选择排序排序后：", seed)
+}
+
+func TestSelectionMinSort(t *testing.T) {
+	//seed := tools.Random(4, 40, 5)
+	seed := []int{2, 3, 1, 4, 5}
+	fmt.Println("选择排序排序前：", seed)
+	selection.SelectionMinSort(seed)
+}
+
+func TestMax(t *testing.T) {
+	//seed := tools.Random(4, 40, 5)
+	seed := []int{2, 3, 1, 5, 4}
+	fmt.Println("选择排序排序前：", seed)
+	selection.Max(seed)
 }
